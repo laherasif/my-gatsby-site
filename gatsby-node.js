95b4ac16d17cfda854bfec60151d6eaca1c9107f
laherasif <laherasif@gmail.com>
2021-05-10 23:03:46 +0500

    updated

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,35 +1,78 @@
-const path = require(`path`);
+// const path = require(`path`);
+// exports.createPages = async ({actions, graphql, reporter}) => {
+//   const {createPage} = actions;
 
-exports.createPages = async ({actions, graphql, reporter}) => {
-  const {createPage} = actions;
+//   const blogPostTemplate = path.resolve(`src/templates/blog-post.js`);
 
-  const blogPostTemplate = path.resolve(`src/templates/blog-post.js`);
+//   const result = await graphql(`
+//     {
+//       allMarkdownRemark(
+//         sort: { order: DESC, fields: [frontmatter___date] }
+//         limit: 1000
+//       ) {
+//         edges {
+//           node {
+//             frontmatter {
+//               path
+//             }
+//           }
+//         }
+//       }
+//     }
+//   `);
+//   if (result.errors) {
+//     reporter.panicOnBuild(`Error while running GraphQL query.`);
+//     return
+//   }
 
-  const result = await graphql(`
-    {
-      allMarkdownRemark(
-        sort: { order: DESC, fields: [frontmatter___date] }
-        limit: 1000
-      ) {
-        edges {
-          node {
-            frontmatter {
-              path
-            }
-          }
-        }
-      }
-    }
-  `);
+ 
 
-  // Handle errors
-  if (result.errors) {
-    reporter.panicOnBuild(`Error while running GraphQL query.`);
-    return
-  }
-
-  console.log("result " ,  result.data.allMarkdownRemark.edges);
+//   result.data.allMarkdownRemark.edges.forEach((obj)=>{
+//     createPage({
+//         path: `/blog/${obj.title}`,
+//         component: path.resolve(`src/templates/blog-post.js`),
+//         context: {}
+//     })
+// })
   
 
  
-};
+// };
+
+
+
+
+var path = require('path');
+
+exports.createPages = async ({actions, graphql}) => {
+    const {createPage} = actions;
+
+    const result = await graphql(`
+         {
+           allMarkdownRemark(
+             sort: { order: DESC, fields: [frontmatter___date] }
+             limit: 1000
+           ) {
+             edges {
+               node {
+                 frontmatter {
+                   path
+                 }
+               }
+             }
+           }
+         }
+       `);
+
+
+    result.data.allMarkdownRemark.edges.forEach((obj)=>{
+        createPage({
+            path: `/books/${obj.title}`,
+            component: path.resolve(`src/template/blog-post.js`),
+            context: {
+                itemDetails: obj
+            }
+        })
+    })
+}
+
