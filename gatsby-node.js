c02efcb3ea0180ad91e4d06049c818762d4503ae
laherasif <laherasif@gmail.com>
2021-05-10 23:27:31 +0500

    changes

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,78 +1,44 @@
-// const path = require(`path`);
-// exports.createPages = async ({actions, graphql, reporter}) => {
-//   const {createPage} = actions;
+const path = require(`path`);
+exports.createPages = async ({actions, graphql, reporter}) => {
+  const {createPage} = actions;
 
-//   const blogPostTemplate = path.resolve(`src/templates/blog-post.js`);
+  const blogPostTemplate = path.resolve(`src/templates/blog-post.js`);
 
-//   const result = await graphql(`
-//     {
-//       allMarkdownRemark(
-//         sort: { order: DESC, fields: [frontmatter___date] }
-//         limit: 1000
-//       ) {
-//         edges {
-//           node {
-//             frontmatter {
-//               path
-//             }
-//           }
-//         }
-//       }
-//     }
-//   `);
-//   if (result.errors) {
-//     reporter.panicOnBuild(`Error while running GraphQL query.`);
-//     return
-//   }
+  const result = await graphql(`
+    {
+      allMarkdownRemark(
+        sort: { order: DESC, fields: [frontmatter___date] }
+        limit: 1000
+      ) {
+        edges {
+          node {
+            frontmatter {
+              path
+            }
+          }
+        }
+      }
+    }
+  `);
+  if (result.errors) {
+    reporter.panicOnBuild(`Error while running GraphQL query.`);
+    return
+  }
 
  
 
-//   result.data.allMarkdownRemark.edges.forEach((obj)=>{
-//     createPage({
-//         path: `/blog/${obj.title}`,
-//         component: path.resolve(`src/templates/blog-post.js`),
-//         context: {}
-//     })
-// })
+  result.data.allMarkdownRemark.edges.forEach((obj)=>{
+    createPage({
+        path: `/blog/${obj.title}`,
+        component: path.resolve(`src/template/blog-post.js`),
+        context: {}
+    })
+})
   
 
  
-// };
+};
 
 
 
-
-var path = require('path');
-
-exports.createPages = async ({actions, graphql}) => {
-    const {createPage} = actions;
-
-    const result = await graphql(`
-         {
-           allMarkdownRemark(
-             sort: { order: DESC, fields: [frontmatter___date] }
-             limit: 1000
-           ) {
-             edges {
-               node {
-                 frontmatter {
-                   path
-                 }
-               }
-             }
-           }
-         }
-       `);
-
-
-    result.data.allMarkdownRemark.edges.forEach((obj)=>{
-        createPage({
-            path: `/books/${obj.title}`,
-            component: path.resolve(`src/template/blog-post.js`),
-            context: {
-                itemDetails: obj
-            }
-        })
-    })
-}
 
--- src/template/blog-post.js
+++ src/template/blog-post.js
@@ -1,83 +1,32 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Link, graphql, StaticQuery } from 'gatsby'
-
-class BlogRoll extends React.Component {
-  render() {
-    const { data } = this.props;
-    const { edges: posts } = data.allMarkdownRemark;
-
-    return (
-      <div className="columns is-multiline">
-        {posts &&
-          posts.map(({ node: post }) => (
-            <div className="is-parent column is-6" key={post.id}>
-              <article
-                className={`blog-list-item tile is-child box notification ${
-                  post.frontmatter.featuredpost ? 'is-featured' : ''
-                }`}
-              >
-                <header>
-                  <p className="post-meta">
-                    <Link
-                      className="title has-text-primary is-size-4"
-                      to={post.frontmatter.path}
-                    >
-                      {post.frontmatter.title}
-                    </Link>
-                    <span> &bull; </span>
-                    <span className="subtitle is-size-5 is-block">
-                      {post.frontmatter.date}
-                    </span>
-                  </p>
-                </header>
-                <p>
-                  {post.excerpt}
-                  <br />
-                  <br />
-                  <Link className="button" to={post.frontmatter.path}>
-                    Keep Reading →
-                  </Link>
-                </p>
-              </article>
-            </div>
-          ))}
+import React from "react"
+import { graphql } from "gatsby"
+export default function Template({
+  data, // this prop will be injected by the GraphQL query below.
+}) {
+  const { markdownRemark } = data // data.markdownRemark holds your post data
+  const { frontmatter, html } = markdownRemark
+  return (
+    <div className="blog-post-container">
+      <div className="blog-post">
+        <h1>{frontmatter.title}</h1>
+        <h2>{frontmatter.date}</h2>
+        <div
+          className="blog-post-content"
+          dangerouslySetInnerHTML={{ __html: html }}
+        />
       </div>
-    )
-  }
+    </div>
+  )
 }
-
-BlogRoll.propTypes = {
-  data: PropTypes.shape({
-    allMarkdownRemark: PropTypes.shape({
-      edges: PropTypes.array,
-    }),
-  }),
-};
-
-const query = () => (
-  <StaticQuery
-    query={graphql`
-      query BlogRollQuery {
-        allMarkdownRemark(
-          sort: { order: DESC, fields: [frontmatter___date] }
-        ) {
-          edges {
-            node {
-              excerpt(pruneLength: 400)
-              id
-              frontmatter {
-                path
-                title
-                date(formatString: "MMMM DD, YYYY")
-              }
-            }
-          }
-        }
+export const pageQuery = graphql`
+  query($path: String!) {
+    markdownRemark(frontmatter: { path: { eq: $path } }) {
+      html
+      frontmatter {
+        date(formatString: "MMMM DD, YYYY")
+        path
+        title
       }
-    `}
-    render={(data, count) => <BlogRoll data={data} count={count} />}
-  />
-)
-
-export default query
+    }
+  }
+`
